696fc33c531efcd5bfaaa7aaed52c79c6e775fd8
Stéphanie Vanoverberghe <orangestreet59123@gmail.com>
2025-09-28 17:29:06 +0200

    RESET

--- src/app/not-found.tsx
+++
@@ -1,7 +0,0 @@
-export default function NotFound() {
-    return (
-        <div className="text-center p-10">
-            <h1 className="text-2xl font-bold text-red-500">404 - Page non trouvée</h1>
-        </div>
-    );
-}
